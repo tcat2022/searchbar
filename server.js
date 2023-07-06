7f6a34ab35f75abbf06d09e50cf9436363ad99c4
tommy <tcatty7@gmail.com>
2023-07-06 12:14:50 -0400

    f

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server, {cors: {origin: "*"}})
+const cors = require('cors')
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+const rooms = {}
+
+app.get('/', (req, res) => {
+res.render('index', {rooms: rooms })
+})
+
+app.get('/:room', (res,req) => {
+    res.render('room', {roomName: req.params.room })
+}) 
+
+server.listen(3000)
+
+const users = {}
+
+io.on('connection', socket => {
+    socket.on('new-user', name => {
+users[socket.id] = name
+socket.broadcast.emit('user-connected', name)
+    })
+    socket.on('send-chat-message', message => {
+       socket.broadcast.emit('chat-message',{ message: message, name: users[socket.id] })
+    })
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+      delete users[socket.id]
+   })
+})
+
