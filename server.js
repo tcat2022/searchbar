f121cd928a7e0b7e49cc720258031d0650bae5bc
tommy <tcatty7@gmail.com>
2023-07-06 12:12:31 -0400

    f

--- server.js
+++
@@ -1,7 +0,0 @@
-var cors = require('cors');
-app.use(cors({origin: "*"}));
-const io = require('socket.io')(3000);
-
-io.on('connection', socket => {
-    socket.emit('chat-message', 'hello world')
-})
