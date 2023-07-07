6b4a98488c7b721bbab8843d358117fdce2da6b0
tommy <tcatty7@gmail.com>
2023-07-07 18:57:40 -0400

    testing

--- public/script.js
+++ public/script.js
@@ -1,32 +1,11 @@
 const socket = io('https://catchat-dlp8.onrender.com');
 const messageContainer = document.getElementById('message-container');
-const roomContainer = document.getElementById('room-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 
-if(messageForm != null) {
-    const name = prompt('what is your name')
-    appendMessage('You joined')
-    socket.emit('new-user',roomName, name )
-
-    messageForm.addEventListener('submit', e => {
-        e.preventDefault();
-        const message = messageInput.value
-        appendMessage(`You: ${message}`)
-        socket.emit('send-chat-message',roomName, message);
-        messageInput.value = ''
-        })
-}
-
-socket.on('room-created', room => {
-    const roomElement = document.createElement('div')
-    roomElement.innerText = room
-    const roomLink = document.createElement('a')
-    roomLink.href = `/${room}`
-    roomLink.innerText = 'join'
-    roomContainer.append(roomElement)
-    roomContainer.append(roomLink)
-})
+const name = prompt('what is your name')
+appendMessage('You joined')
+socket.emit('new-user', name )
 
 socket.on('chat-message', data =>{
     appendMessage(`${data.name}: ${data.message}`)
@@ -38,6 +17,14 @@ socket.on('user-connected', name =>{
 
 socket.on('user-disconnected', name =>{
     appendMessage(`${name} disconnected`)
+})
+
+messageForm.addEventListener('submit', e => {
+e.preventDefault();
+const message = messageInput.value
+appendMessage(`You: ${message}`)
+socket.emit('send-chat-message', message);
+messageInput.value = ''
 })
 
 function appendMessage(message){
